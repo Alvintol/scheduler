13284e24f9e6f33899c659c14b459da5a7886fd8
Alvin <alvin.tol@hotmail.com>
2022-04-19 10:43:58 -0600

    adds dayList component and respective css

---
+++ src/components/DayListItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function DayListItem(props) {
+
+  return (
+    <li onClick={() => {
+      props.setDay(props.name)
+    }}>
+      <h2 className="text--regular">
+        {props.name}
+      </h2>
+      <h3 className="text--light">
+        {props.spots} spots remaining
+      </h3>
+    </li>
+  );
+}
