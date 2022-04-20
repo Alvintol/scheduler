bc93c4a1fb2a4dd3cef7624756b1fdd23e1eaada
Alvin <alvin.tol@hotmail.com>
2022-04-20 17:05:13 -0600

    adds show & confirm components with respective stories

---
+++ src/components/Appointment/Status.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Status = (props) => {
+  
+};
+
+export default Status;
--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index.js";
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
 
 storiesOf("Button", module)
   .addParameters({
@@ -150,21 +151,26 @@ storiesOf("InterviewerList", module)
     />
   ));
 
-  storiesOf("Appointment", module)
+storiesOf("Appointment", module)
   .addParameters({
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => 
-  <Header 
-    time='12pm' />)
-  .add("Empty", () => 
-  <Empty 
-    onAdd={action("onAdd")} />)
-  .add("Empty", () => 
-  <Show 
-    onEdit={action("onEdit")} 
-    onDelete={action("onDelete")} />)
+  .add("Header", () =>
+    <Header
+      time='12pm' />)
+  .add("Empty", () =>
+    <Empty
+      onAdd={action("onAdd")} />)
+  .add("Show", () =>
+    <Show
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")} />)
+  .add("Confirm", () =>
+    <Confirm
+      message='Delete the appointment?'
+      onConfirm={action("onConfirm")}
+      onCancel={action("onCancel")} />)
   ;
 
