e0b9dc7fd430bf321b220237ac653e48838c62fb
Alvin <alvin.tol@hotmail.com>
2022-04-19 20:28:50 -0600

    addslist of days in side bar and respective hover states

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,16 +1,31 @@
 import React from "react";
+import classNames from 'classnames';
+import 'components/DayListItem.scss'
 
 export default function DayListItem(props) {
 
+  let dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots === 0
+  });
+
+  const formatSpots = (spots) => {
+    switch (spots) {
+      case 0: return 'no spots remaining';
+      case 1: return '1 spot remaining';
+      default: return `${spots} spots remaining`;
+    }
+  }
+
   return (
-    <li onClick={() => {
+    <li className={dayClass} onClick={() => {
       props.setDay(props.name)
     }}>
       <h2 className="text--regular">
         {props.name}
       </h2>
       <h3 className="text--light">
-        {props.spots} spots remaining
+        {formatSpots(props.spots)}
       </h3>
     </li>
   );
