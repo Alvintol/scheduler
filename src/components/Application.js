892e43f4892367d0da03b469e9f9e8be071ff4df
Alvin <alvin.tol@hotmail.com>
2022-04-21 23:04:38 -0600

    adds api access to days, appointments and interviewers

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,61 +3,22 @@ import "components/Application.scss";
 import DayList from './DayList';
 import Appointment from './Appointment';
 import axios from 'axios';
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
-
+import { getAppointmentsForDay } from './helpers/selectors';
 
 export default function Application() {
 
-  const [appointments, setAppointments] = useState({})
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {}
   });
+  const [appointments, setAppointments] = useState({})
 
   const setDay = day => setState({ ...state, day });
   const setDays = days => setState(prev => ({ ...prev, days }));
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
-  const eachAppointment = Object.values(appointments).map(appointment => {
+  const eachAppointment = dailyAppointments.map(appointment => {
     return (
       <Appointment
         key={appointment.id}
@@ -67,11 +28,13 @@ export default function Application() {
   });
 
   useEffect(() => {
-    axios
-      .get("/api/days")
-      .then((response) => {
-        setDays(response.data)
-      })
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+    ]).then((all) => {
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+    })
       .catch((error) => {
         console.log(error.response.status);
         console.log(error.response.headers);
