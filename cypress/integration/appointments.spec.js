01f852fda97d320c2cb213341708fac9b1d7ab6f
Alvin <alvin.tol@hotmail.com>
2022-04-27 15:22:21 -0600

    adds database reset to cypress appointment test

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,19 @@
+describe('Appointments', () => {
+
+  it("should book an interview", () => {
+    cy.request("GET", "/api/debug/reset");
+    
+    cy.visit("/");
+    cy.contains('[data-testid=day]', "Monday");
+    cy.get('[alt=Add]')
+      .first()
+      .click();
+    cy.get("[data-testid=student-name-input]")
+      .type("Lydia Miller-Jones");
+    cy.get('[alt="Sylvia Palmer"]')
+      .click();
+    cy.get('.button--confirm')
+      .click();
+  });
+
+})
