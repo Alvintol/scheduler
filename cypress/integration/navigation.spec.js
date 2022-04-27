8246e54ba00777746e1cc0692b007c0e3cb01c18
Alvin <alvin.tol@hotmail.com>
2022-04-27 14:28:13 -0600

    adds test for Tuesday navigation

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,16 @@
 describe("Navigation", () => {
+
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy.get('li')
+      .contains('Tuesday')
+      .click()
+  });
+
+
+
 });
