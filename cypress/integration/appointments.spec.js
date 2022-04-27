b0f6d06239d0bea6e28d69a88a1b6805c374f4e0
Alvin <alvin.tol@hotmail.com>
2022-04-27 15:48:22 -0600

    adds book an interview cypress test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,19 +1,42 @@
 describe('Appointments', () => {
 
-  it("should book an interview", () => {
+  beforeEach(() => {
     cy.request("GET", "/api/debug/reset");
-    
     cy.visit("/");
     cy.contains('[data-testid=day]', "Monday");
+
+  })
+
+  it("should book an interview", () => {
     cy.get('[alt=Add]')
       .first()
       .click();
+
     cy.get("[data-testid=student-name-input]")
-      .type("Lydia Miller-Jones");
+      .type("Capitan UnderPants");
+
     cy.get('[alt="Sylvia Palmer"]')
       .click();
+
     cy.get('.button--confirm')
       .click();
+
+    cy.contains(".appointment__card--show", "Capitan UnderPants");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {
+    cy.get('[alt=Edit]')
+      .first()
+      .click({ force: true })
+
+    cy.get("[data-testid=student-name-input]").clear().type("Sir Meowzer");
+    cy.get("[alt='Tori Malcolm']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Sir Meowzer");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 
 })
