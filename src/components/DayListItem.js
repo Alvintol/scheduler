c768bc6e1e8e9fb3c48f45e17c9b2541fdf8cfb1
Alvin <alvin.tol@hotmail.com>
2022-04-27 23:48:23 -0600

    Revert "adds SHH keys to github and circleci" (#5)

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -24,9 +24,7 @@ export default function DayListItem(props) {
   }
 
   return (
-    <li 
-    data-testid={'day'} 
-    className={dayClass} onClick={() => {
+    <li data-testid={'day'} className={dayClass} onClick={() => {
       setDay(name)
     }}>
       <h2 className='text--regular'>
